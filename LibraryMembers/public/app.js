dc2f8efe4ffd80c9fab37ebce76ae3a0783cc38c
Spengergasse <HER231037@spengergasse.at>
2025-11-21 23:44:28 +0100

    added Basic-Frontend (HTML, CSS, JS) from sketch

---
+++ LibraryMembers/public/app.js
@@ -0,0 +1,55 @@
+//sobald das HTML geladen wurde, wird dieses JS ausgeführt.
+async function fetchLibraryMember() {
+    //Zugriff auf das Element mit der ID "Status" im DOM
+    const statusEL = document.getElementById("status");
+    //Zugriff auf das tbody der Table mit der ID "member-table"
+    const tbody = document.querySelector("#member-table tbody");
+
+    try{
+        //hier wird der Text überschrieben
+        statusEL.textContent = "Load data...";
+        
+        //fetch(): Der Browser schickt eine Anfrage an diese URL
+        //und gibt die Antwort zurück
+        //wait: warte, bis der Browser dir die Daten geliefert hat
+        //bevor es mit dem Code weitergeht
+        const res = await fetch("/LibraryMember");
+        //res.ok ist true, wenn statuscode 200-299
+        //bei 404 oder 500 -> false , dann wird der error gecatcht.
+        if(!res.ok) throw new Error(`HTTP ${res.status}`);
+        //Antwort vom Server wird in JS-Objekte umgewandelt.
+        const members = await res.json();
+        //tabelle wird zuerst geleert, um etwaige alte Daten zu entfernen
+        tbody.innerHTML = "";
+        
+        for(const m of members) {
+            //erstellt eine neue Zeile
+            const tr = document.createElement("tr");
+            //erstellt neue Spalten
+            const tdId = document.createElement("td");
+            //holt sich die id vom Objekt
+            tdId.textContent = m.id;
+            
+            const tdName = document.createElement("td");
+            tdName.textContent = m.name;
+
+            const tdMember_id = document.createElement("td");
+            tdMember_id.textContent = m.member_id;
+            //Fügt die Zellen in die Zeile hinzu
+            tr.append(tdId, tdName, tdMember_id);
+            //Fügt die Zeile in die Tabelle hinzu
+            tbody.appendChild(tr);
+        }
+        //Status-Text wird aktualisiert. Die Länge des Arrays wird hergeholt
+        statusEL.textContent = `Loaded: ${members.length} LibraryMembers`;
+    }
+    catch(err){
+        //Fehler werden in der Konsole angezeigt
+        console.error(err);
+        //Dementsprechende Meldung sieht der Nutzer
+        statusEL.textContent = "Error while loading the data.";
+    }
+}
+//Wurde die Seite vollständig geladen, dann wird das JS ausgeführt
+//und die Tabelle erstellt und befüllt.
+window.addEventListener("DOMContentLoaded", fetchLibraryMember);
--- LibraryMembers/server.js
+++ LibraryMembers/server.js
@@ -4,6 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 const LibraryMember = [
   {id: 1,   name: "Lukas",      member_id: "MEM001"},
   {id: 2,   name: "Patrick",    member_id: "MEM002"},
